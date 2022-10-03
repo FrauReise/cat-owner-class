c30ccddd92459fd3ef818eb9e74a5160085224fc
Ekaterina Kotenko <104442215+FrauReise@users.noreply.github.com>
2022-10-04 00:43:30 +0300

    corrected errors

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,56 +6,60 @@ sendBtn.addEventListener("click", () => {
     createObjectOwner();
 });
 
-function createObjectCat(){
-    class Cat{
-        constructor(nickname, sex, breed, food){
-            this.nickname = nickname; 
-            this.sex = sex; 
-            this.breed = breed; 
-            this.food = food; 
-        }
+class Cat {
+    constructor(nickname, sex, breed, food) {
+        this.nickname = nickname;
+        this.sex = sex;
+        this.breed = breed;
+        this.food = food;
     }
+}
 
-    let nickname = document.getElementById('nickname').value;
+function createObjectCat() {
 
-    let sex = null; 
-    let elementSex = document.getElementsByName('sex');
-    for(let i=0; elementSex[i]; ++i){
-        if(elementSex[i].checked){
-            sex = elementSex[i].value;
-            break;
-        }
-    }
-
-    let breed = document.getElementById('breed').value;
-
-    let food = " ";
-    let elementFood = document.getElementsByName('food');
-    for(let i=0; elementFood[i]; ++i){
-        if(elementFood[i].checked){
-            food = `${elementFood[i].value} ${food}`;
-        }
-    }
     let cat = new Cat(nickname, sex, breed, food);
     console.log("Кот")
     console.log(`Кличка: ${cat.nickname}`);
     console.log(`Пол: ${cat.sex}`);
-    console.log(`Порода: ${cat.breed}`); 
+    console.log(`Порода: ${cat.breed}`);
     console.log(`Питание: ${cat.food}`);
 }
 
-function createObjectOwner(){
-    class Owner{
-        constructor(name, phone){
-            this.name = name;
-            this.phone = phone; 
-        }
+
+let nickname = document.getElementById('nickname').value;
+
+let sex = null;
+let elementSex = document.getElementsByName('sex');
+for (let i = 0; elementSex[i]; ++i) {
+    if (elementSex[i].checked) {
+        sex = elementSex[i].value;
+        break;
     }
+}
+
+let breed = document.getElementById('breed').value;
+
+let food = " ";
+let elementFood = document.getElementsByName('food');
+for (let i = 0; elementFood[i]; ++i) {
+    if (elementFood[i].checked) {
+        food = `${elementFood[i].value} ${food}`;
+    }
+}
+
+class Owner {
+    constructor(name, phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+}
+
+function createObjectOwner() {
     let name = document.getElementById('firstname').value;
     let phone = document.getElementById('phone').value;
 
     let owner = new Owner(name, phone);
     console.log("Владелец кота")
     console.log(`Имя: ${owner.name}`);
-    console.log(`Телефон: ${owner.phone}`);  
+    console.log(`Телефон: ${owner.phone}`);
 }
